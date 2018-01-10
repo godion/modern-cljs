708668022281eff4004342857a5ba1284a228605
godion <dionchen@outlook.com>
2018-01-10 09:29:59 +0800

    Simple made easy.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [compojure "1.6.0"]]
+                 [compojure "1.6.0"]
+                 [domina "1.0.3"]]
   :source-paths ["src/clj" "src/cljs"]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-ring "0.12.3"]]
--- src/cljs/modern_cljs/login.cljs
+++ src/cljs/modern_cljs/login.cljs
@@ -1,4 +1,5 @@
-(ns modern-cljs.login)
+(ns modern-cljs.login
+  (:use [domina :only [by-id value]]))
 
 (defn validate-form []
   (let [email (.getElementById js/document "email")
@@ -9,9 +10,21 @@
       (do (js/alert "Please, complete the form!")
           false))))
 
+(defn validate-form []
+  ;; get email and password element using (by-id id)
+  (let [email (by-id "email")
+        password (by-id "password")]
+    ;; get email and password value using (value el)
+    (if (and (> (count (value email)) 0)
+             (> (count (value password)) 0))
+      true
+      (do (js/alert "Please, complete the form!")
+          false))))
+
+
 ;; define the function to attach validate-form to onsubmit event of
 ;; the form
-(defn init []
+(defn ^:export init []
   ;; verify that js/document exists and that it has a getElementById
   ;; property
   (if (and js/document
@@ -22,5 +35,5 @@
       (set! (.-onsubmit login-form) validate-form))))
 
 
-(set! (.-onload js/window) init)
+;(set! (.-onload js/window) init)
 
